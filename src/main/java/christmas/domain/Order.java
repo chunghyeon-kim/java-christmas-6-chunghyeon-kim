5fb72803da7e38303bf706bb258e22267bb7a603
chunghyeon-kim <chunghyeonkim90@gmail.com>
2023-11-10 03:49:59 +0900

    refactor(상수): 상수 이름 변경

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -7,8 +7,8 @@ import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Order {
-    private static final int TOTAL_MENU_UPPER_LIMIT = 20;
-    private static final String MENU_OVER_LIMIT = "[ERROR] 메뉴는 20개를 초과할 수 없습니다.";
+    private static final int TOTAL_MENU_UPPER_BOUND = 20;
+    private static final String MENU_OVER_BOUND = "[ERROR] 메뉴는 20개를 초과할 수 없습니다.";
 
     private final Map<Orderable, Integer> contents = new HashMap<>();
 
@@ -20,8 +20,8 @@ public class Order {
         Collection<Integer> quantities = contents.values();
         AtomicInteger count = new AtomicInteger();
         quantities.forEach(count::addAndGet);
-        if (count.get() > TOTAL_MENU_UPPER_LIMIT) {
-            throw new IllegalArgumentException(MENU_OVER_LIMIT);
+        if (count.get() > TOTAL_MENU_UPPER_BOUND) {
+            throw new IllegalArgumentException(MENU_OVER_BOUND);
         }
     }
 
