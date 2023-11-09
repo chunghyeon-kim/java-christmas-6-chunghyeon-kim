04fc472cafa5dd71976c09ce6aae09a8deb8a7a6
chunghyeon-kim <chunghyeonkim90@gmail.com>
2023-11-10 05:05:57 +0900

    fix(날짜): 날짜 클래스에 유효성검사 추가

--- src/main/java/christmas/domain/DecemberDate.java
+++ src/main/java/christmas/domain/DecemberDate.java
@@ -1,14 +1,14 @@
 package christmas.domain;
 
-public class DecemberDate {
-    private final int date;
+public record DecemberDate(int date) {
+    private static final int FIRST_DAY_OF_DECEMBER = 1;
+    private static final int LAST_DAY_OF_DECEMBER = 31;
+    private static final String WRONG_DATE = "[ERROR] 잘못된 날짜입니다.";
 
-    public DecemberDate(int date) {
-        this.date = date;
-    }
-
-    public int getDate() {
-        return date;
+    public DecemberDate {
+        if (date < FIRST_DAY_OF_DECEMBER || LAST_DAY_OF_DECEMBER < date) {
+            throw new IllegalArgumentException(WRONG_DATE);
+        }
     }
 
 }
