2b52558ffe2c7d8cab1f9d7a0a9a6a5c21555c06
chunghyeon-kim <chunghyeonkim90@gmail.com>
2023-11-11 04:18:12 +0900

    refactor(기능): 메뉴 20개 초과한지 확인하는 메서드 로직 변경

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -2,11 +2,9 @@ package christmas.domain;
 
 import christmas.domain.constant.dish.Beverage;
 import christmas.domain.constant.dish.Orderable;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Order {
     private static final int TOTAL_MENU_UPPER_BOUND = 20;
@@ -20,10 +18,10 @@ public class Order {
     }
 
     public void isOverLimit() {
-        Collection<Integer> quantities = contents.values();
-        AtomicInteger count = new AtomicInteger();
-        quantities.forEach(count::addAndGet);
-        if (count.get() > TOTAL_MENU_UPPER_BOUND) {
+        int allDishes = contents.values().stream()
+                .mapToInt(Integer::valueOf)
+                .sum();
+        if (allDishes > TOTAL_MENU_UPPER_BOUND) {
             throw new IllegalArgumentException(MENU_OVER_BOUND);
         }
     }
