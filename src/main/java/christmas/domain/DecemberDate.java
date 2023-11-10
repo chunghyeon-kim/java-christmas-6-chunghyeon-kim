8735d7d62dbc0ec6ee3525da5727c3eed62c86c1
chunghyeon-kim <chunghyeonkim90@gmail.com>
2023-11-11 05:30:45 +0900

    feat(입력): 12월 중 식당 예상 방문 날짜를 입력받는다. 사용자가 입력하는 값은 `camp.nextstep.edu.missionutils.Console`의 `readLine()`을 활용한다.
    하위기능
    - 1 이상 31 이하의 숫자가 아닌 경우 에러 메세지를 출력하고 다시 입력받는다. `[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.`
    - 모든 에러 메시지는 "[ERROR]"로 시작하도록 작성한다.

--- src/main/java/christmas/domain/DecemberDate.java
+++ src/main/java/christmas/domain/DecemberDate.java
@@ -3,7 +3,7 @@ package christmas.domain;
 public record DecemberDate(int date) {
     private static final int FIRST_DAY_OF_DECEMBER = 1;
     private static final int LAST_DAY_OF_DECEMBER = 31;
-    private static final String WRONG_DATE = "[ERROR] 잘못된 날짜입니다.";
+    private static final String WRONG_DATE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
 
     public DecemberDate {
         if (date < FIRST_DAY_OF_DECEMBER || LAST_DAY_OF_DECEMBER < date) {
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,4 +1,32 @@
 package christmas.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import christmas.domain.DecemberDate;
+
 public class InputView {
+    private static final String VISIT_DATE_CALL_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String VISIT_DATE_SHOULD_NOT_CHARACTER = "[ERROR] 방문 날짜는 숫자만 입력할 수 있습니다.";
+
+    public DecemberDate getVisitDate() {
+        System.out.println(VISIT_DATE_CALL_MESSAGE);
+        DecemberDate visitDate;
+        do {
+            visitDate = getVisitDateInput();
+        } while(visitDate == null);
+
+        return visitDate;
+    }
+
+    private DecemberDate getVisitDateInput() {
+        try {
+            int input = Integer.parseInt(Console.readLine().trim());
+            return new DecemberDate(input);
+        } catch (NumberFormatException nfe) {
+            System.out.println(VISIT_DATE_SHOULD_NOT_CHARACTER);
+        } catch (IllegalArgumentException ie) {
+            System.out.println(ie.getMessage());
+        }
+        return null;
+    }
+
 }
