a9772998b9a35d682cd60a0de3212ac5f0d18f64
chunghyeon-kim <chunghyeonkim90@gmail.com>
2023-11-12 02:17:57 +0900

    refactor(테스트): PresentationManagerTest 상수 추가

--- src/test/java/christmas/service/PresentationManagerTest.java
+++ src/test/java/christmas/service/PresentationManagerTest.java
@@ -10,10 +10,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PresentationManagerTest {
+    private static final int PRESENTATION_THRESHOLD = 120000;
+    private static final int ONE = 1;
+
     @DisplayName("할인 전 총주문 금액이 12만원 이상인 경우 샴페인을 증정한다.")
     @Test
     void presentChampagne() {
-        BenefitDto dto = new BenefitDto(new HashMap<>(), 120000);
+        BenefitDto dto = new BenefitDto(new HashMap<>(), PRESENTATION_THRESHOLD);
         PresentationManager.present(dto);
 
         assertThat(dto.getBenefitMap()).containsKey(Benefit.PRESENTATION);
@@ -23,10 +26,10 @@ class PresentationManagerTest {
     @DisplayName("할인 전 총주문 금액이 12만원 미만인 경우 샴페인을 증정하지 않는다.")
     @Test
     void notPresentChampagne() {
-        BenefitDto dto = new BenefitDto(new HashMap<>(), 120000 - 1);
+        BenefitDto dto = new BenefitDto(new HashMap<>(), PRESENTATION_THRESHOLD - ONE);
         PresentationManager.present(dto);
 
-        assertThat(dto.getBenefitMap().containsKey(Benefit.PRESENTATION)).isFalse();
+        assertThat(dto.getBenefitMap()).doesNotContainKey(Benefit.PRESENTATION);
         assertThat(dto.getPresentation()).isNull();
     }
 
